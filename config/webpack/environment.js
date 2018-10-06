ef2691148fec31762be623020844e9c0e89155d2
Matt Lewis <matt@mplewis.com>
2018-10-06 17:53:15 -0600

    feat: yaml load

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,9 @@
 const { environment } = require('@rails/webpacker')
-const vue =  require('./loaders/vue')
+const vue = require('./loaders/vue')
 
 environment.loaders.append('vue', vue)
+environment.loaders.append('yml', {
+  test: /\.yml$/,
+  use: [{ loader: 'json-loader' }, { loader: 'yaml-loader' }]
+})
 module.exports = environment
