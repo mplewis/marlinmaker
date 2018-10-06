ba3252f88f4e6bf9d7197b7b1f85dee06b6c886a
Matt Lewis <matt@mplewis.com>
2018-10-06 17:53:26 -0600

    feat: settings from yml

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,27 +1,26 @@
 <template>
-<div class="container">
+<div class="container my-4">
   <div class="row">
     <div class="col-sm">
-      <p>{{ message }}</p>
-      <b-button variant="success">
-        BootstrapVue works!
-      </b-button>
+      <b-form-select v-model="board_name" :options="settings.board_name.options" class="mb-3" />
+      <b-form-input v-model="ezabl_points" placeholder="EZABL Points" type="number" class="mb-3" />
+      <b-form-checkbox v-model="ezabl_outside_grid_compensation" class="mb-3">EZABL: Outside Grid Compensation</b-form-checkbox>
+      <b-form-select v-model="lcd_language" :options="settings.lcd_language.options" class="mb-3" />
     </div>
   </div>
 </div>
 </template>
 
 <script>
+const settings = require('./settings.yml')
+
 export default {
-  data: function() {
-    return {
-      message: "Hello Vue!"
-    }
-  }
+  data: () => ({
+    settings,
+    board_name: null,
+    ezabl_points: 5,
+    ezabl_outside_grid_compensation: true,
+    lcd_language: 'en',
+  })
 }
 </script>
-
-<style scoped lang="stylus">
-p
-  color: navy
-</style>
