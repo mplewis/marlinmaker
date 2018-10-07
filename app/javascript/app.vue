d4788f65d9fb8e4814cf2d2a7cb6fc0929d4e3d8
Matt Lewis <matt@mplewis.com>
2018-10-07 01:22:51 -0600

    feat: better form layout

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -3,14 +3,20 @@
   <div class="row">
     <div class="col-sm">
       <b-form>
-        <div v-for="(spec, key) in options" :key="key" class="mb-3">
-          <b-form-group :label="spec.desc" :label-for="key">
+        <b-row v-for="(spec, key) in options" :key="key" class="mb-3">
+          <b-col cols="4" class="text-right">
+            <b-form-group :label="spec.desc" :label-for="key" class="mb-0" />
+          </b-col>
+          <b-col cols="8">
             <b-form-checkbox v-if="spec.type === 'boolean'" v-model="selected[key]">Enable</b-form-checkbox>
-            <b-form-input v-else-if="['integer', 'float'].includes(spec.type)" v-model="selected[key]" type="number" />
+            <b-form-input v-else-if="spec.type === 'integer'" v-model="selected[key]" type="number" pattern="\d*" />
+            <b-form-input v-else-if="spec.type === 'float'" v-model="selected[key]" type="number" />
             <b-form-select v-else-if="spec.type === 'select'" v-model="selected[key]" :options="spec.options" />
-            <b-alert v-else variant="danger" show>Could not generate form field of type {{spec.type}} for {{key}}</b-alert>
-          </b-form-group>
-        </div>
+            <b-alert v-else variant="danger" show>
+              Could not generate form field of type {{spec.type}} for {{key}}
+            </b-alert>
+          </b-col>
+        </b-row>
       </b-form>
     </div>
   </div>
