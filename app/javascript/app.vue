54200da1c319b327c3fb64f1a06206780356b82e
Matt Lewis <matt@mplewis.com>
2018-10-07 00:31:04 -0600

    feat: better form with descs

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -2,11 +2,16 @@
 <div class="container my-4">
   <div class="row">
     <div class="col-sm">
-      <div v-for="(spec, key) in options" :key="key" class="mb-3">
-        <b-form-checkbox v-if="spec.type === 'boolean'" v-model="selected[key]">{{spec.desc}}</b-form-checkbox>
-        <b-form-input v-if="spec.type === 'number'" v-model="selected[key]" type="number" :placeholder="spec.desc" />
-        <b-form-select v-if="spec.type === 'select'" v-model="selected[key]" :options="spec.options" />
-      </div>
+      <b-form>
+        <div v-for="(spec, key) in options" :key="key" class="mb-3">
+          <b-form-group :label="spec.desc" :label-for="key">
+            <b-form-checkbox v-if="spec.type === 'boolean'" v-model="selected[key]">Enable</b-form-checkbox>
+            <b-form-input v-else-if="['integer', 'float'].includes(spec.type)" v-model="selected[key]" type="number" />
+            <b-form-select v-else-if="spec.type === 'select'" v-model="selected[key]" :options="spec.options" />
+            <b-alert v-else variant="danger" show>Could not generate form field of type {{spec.type}} for {{key}}</b-alert>
+          </b-form-group>
+        </div>
+      </b-form>
     </div>
   </div>
 </div>
