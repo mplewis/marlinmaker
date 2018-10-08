49cd3515db79634d113ffb072858fbc98df18628
Matt Lewis <matt@mplewis.com>
2018-10-07 21:06:57 -0600

    feat: all grouped options, layout

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -6,21 +6,25 @@
         <div v-for="(groupInfo, groupName) in groupsWithOptions" :key="groupName">
           <div class="group-title">
             <h1 class="mb-0">{{groupName}}</h1>
-            <b-btn v-b-toggle="groupName" class="mb-1">Toggle</b-btn>
+            <b-btn v-b-toggle="groupName" size="sm" class="mb-3 ml-3">Toggle</b-btn>
+            <p v-if="groupInfo.desc">{{groupInfo.desc}}</p>
           </div>
-          <p>{{groupInfo.desc}}</p>
           <b-collapse :id="groupName" :visible="groupInfo.startOpen">
             <b-row v-for="(spec) in groupInfo.options" :key="spec.key" class="mb-3">
               <b-col cols="4" class="text-right">
                 <b-form-group :label="spec.desc" :label-for="spec.key" class="mb-0" />
               </b-col>
               <b-col cols="8">
-                <b-form-checkbox v-if="spec.type === 'boolean'" v-model="selected[spec.key]">{{spec.enableVerb}}</b-form-checkbox>
+                <b-alert v-if="!spec.type" variant="danger" show>
+                  No field type found for {{spec.key}}. Is it typed correctly?
+                </b-alert>
+                <b-form-checkbox v-else-if="spec.type === 'boolean'" v-model="selected[spec.key]">{{spec.enableVerb}}</b-form-checkbox>
                 <b-form-input v-else-if="spec.type === 'integer'" v-model="selected[spec.key]" type="number" pattern="\d*" />
                 <b-form-input v-else-if="spec.type === 'float'" v-model="selected[spec.key]" type="number" />
+                <b-form-input v-else-if="spec.type === 'string'" v-model="selected[spec.key]" />
                 <b-form-select v-else-if="spec.type === 'select'" v-model="selected[spec.key]" :options="spec.options" />
                 <b-alert v-else variant="danger" show>
-                  Could not generate form field of type {{spec.type}} for {{spec.key}}
+                  Could not generate form field for {{spec.key}} (type: "{{spec.type}}")
                 </b-alert>
               </b-col>
             </b-row>
